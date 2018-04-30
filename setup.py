b9c21df04a09ef593debc4ca80382a8f9c527171
Gauthier <gauthier@hebus>
2018-04-30 10:54:11 +0200

    add cx_Freeze setup script

---
+++ setup.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'                                                                         '
+' Copyright 2018 Gauthier Brière (gauthier.briere "at" gmail.com)         '
+'                                                                         '
+' This file is part of cn5X++                                               '
+'                                                                         '
+' cn5X++ is free software: you can redistribute it and/or modify it         '
+'  under the terms of the GNU General Public License as published by      '
+' the Free Software Foundation, either version 3 of the License, or       '
+' (at your option) any later version.                                     '
+'                                                                         '
+' cn5X++ is distributed in the hope that it will be useful, but           '
+' WITHOUT ANY WARRANTY; without even the implied warranty of              '
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           '
+' GNU General Public License for more details.                            '
+'                                                                         '
+' You should have received a copy of the GNU General Public License       '
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.   '
+'                                                                         '
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+from cx_Freeze import setup, Executable
+
+setup(
+    name = "cn5X++",
+    version = "1",
+    description = "5/6 axis Grbl control panel for grbl-Mega-5X",
+    executables = [Executable("cn5X.py")],
+)
